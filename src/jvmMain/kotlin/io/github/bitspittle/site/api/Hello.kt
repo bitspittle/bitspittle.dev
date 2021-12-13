54d1efdc25b025ff875b9d6848f52854e89e9b47
David Herman <bitspittle@gmail.com>
2021-12-13 11:18:45 -0800

    Nit: Update generated code to use better Kobweb APIs

--- src/jvmMain/kotlin/io/github/bitspittle/site/api/Hello.kt
+++ src/jvmMain/kotlin/io/github/bitspittle/site/api/Hello.kt
@@ -2,8 +2,9 @@ package io.github.bitspittle.site.api
 
 import com.varabyte.kobweb.api.Api
 import com.varabyte.kobweb.api.ApiContext
+import com.varabyte.kobweb.api.http.setBodyText
 
 @Api
 fun hello(ctx: ApiContext) {
-    ctx.res.body = "hello world".toByteArray()
+    ctx.res.setBodyText("hello world")
 }
