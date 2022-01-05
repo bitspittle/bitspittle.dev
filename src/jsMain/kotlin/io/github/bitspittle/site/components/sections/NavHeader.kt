0af489ed4340883df9a92d576a61728874f73b58
David Herman <bitspittle@gmail.com>
2022-01-05 15:31:13 -0800

    Tweak navheader
    No alternating color, just simple headers with a special color for the logo link

--- src/jsMain/kotlin/io/github/bitspittle/site/components/sections/NavHeader.kt
+++ src/jsMain/kotlin/io/github/bitspittle/site/components/sections/NavHeader.kt
@@ -1,16 +1,14 @@
 package io.github.bitspittle.site.components.sections
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.foundation.layout.Spacer
-import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.core.rememberPageContext
 import com.varabyte.kobweb.silk.components.icons.fa.FaMoon
 import com.varabyte.kobweb.silk.components.icons.fa.FaSun
+import com.varabyte.kobweb.silk.components.icons.fa.FaTwitter
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.navigation.UndecoratedLinkVariant
 import com.varabyte.kobweb.silk.components.style.ComponentStyle
@@ -25,39 +23,34 @@ import com.varabyte.kobweb.silk.theme.shapes.Circle
 import com.varabyte.kobweb.silk.theme.shapes.clip
 import com.varabyte.kobweb.silk.theme.toSilkPalette
 import io.github.bitspittle.site.SitePalettes
+import io.github.bitspittle.site.components.widgets.IconButton
 import org.jetbrains.compose.web.css.*
 
 val NavHeaderStyle = ComponentStyle.base("bs-nav-header") {
     Modifier
         .fillMaxWidth()
-        .height(3.cssRem)
-        // Intentionally invert the nav header color so that it is distinct from the page
-        .backgroundColor(colorMode.toSilkPalette().color)
+        .padding(left = 1.cssRem, right = 1.cssRem, top = 1.cssRem, bottom = 3.cssRem)
+        .fontSize(1.25.cssRem)
 }
-
-private val NAV_ITEM_MARGIN = Modifier.margin(0.px, 15.px)
 
 val NavLinkStyle = ComponentStyle("bs-nav-link") {
-    // Intentionally invert the color for nav links since we inverted the nav header
-    val linkColor = colorMode.toSilkPalette().background
+    val linkColor = colorMode.toSilkPalette().color
 
-    base { NAV_ITEM_MARGIN }
+    base { Modifier.margin(topBottom = 0.px, leftRight = 15.px) }
+
     link { Modifier.color(linkColor) }
     visited { Modifier.color(linkColor) }
 }
 
-val LogoVariant = NavLinkStyle.addVariant("bs-logo") {
-    base {
-        Modifier.fontSize(1.5.cssRem).fontWeight(FontWeight.Bold)
-    }
-    // Intentionally invert the color for nav links since we inverted the nav header
-    val linkColor = SitePalettes[colorMode.opposite()].brand
-    link { Modifier.color(linkColor) }
-    visited { Modifier.color(linkColor) }
+val LogoVariant = NavLinkStyle.addVariant("logo") {
+    val logoColor = SitePalettes[colorMode].brand
+
+    link { Modifier.color(logoColor) }
+    visited { Modifier.color(logoColor) }
 }
 
 val NavButtonStyle = ComponentStyle.base("bs-nav-button") {
-    NAV_ITEM_MARGIN.clip(Circle())
+    Modifier.margin(0.px, 10.px).clip(Circle())
 }
 
 @Composable
@@ -72,24 +65,25 @@ private fun NavLink(path: String, text: String, linkVariant: ComponentVariant? =
 
 @Composable
 fun NavHeader() {
-    var colorMode by rememberColorMode()
-    Row(
-        NavHeaderStyle.toModifier(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+    val ctx = rememberPageContext()
+    Row(NavHeaderStyle.toModifier()) {
         NavLink("/", "\$bs", LogoVariant)
-        NavLink("/about", "ABOUT")
-        NavLink("/markdown", "MARKDOWN")
+        NavLink("/about", "blog")
         Spacer()
-        Button(
+        IconButton(
+            onClick = { ctx.router.navigateTo("https://twitter.com/bitspittle") },
+            NavButtonStyle.toModifier()
+        ) {
+            FaTwitter()
+        }
+        var colorMode by rememberColorMode()
+        IconButton(
             onClick = { colorMode = colorMode.opposite() },
             NavButtonStyle.toModifier()
         ) {
-            Box(Modifier.margin(6.px)) {
-                when (colorMode) {
-                    ColorMode.LIGHT -> FaMoon()
-                    ColorMode.DARK -> FaSun()
-                }
+            when (colorMode) {
+                ColorMode.LIGHT -> FaMoon()
+                ColorMode.DARK -> FaSun()
             }
         }
     }
---
+++ src/jsMain/kotlin/io/github/bitspittle/site/components/widgets/IconButton.kt
@@ -0,0 +1,16 @@
+package io.github.bitspittle.site.components.widgets
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.BoxScope
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.forms.Button
+import org.jetbrains.compose.web.css.*
+
+@Composable
+fun IconButton(onClick: () -> Unit, modifier: Modifier = Modifier, icon: @Composable BoxScope.() -> Unit) {
+    Button(onClick, modifier) {
+        Box(Modifier.padding(8      .px), content = icon)
+    }
+}
