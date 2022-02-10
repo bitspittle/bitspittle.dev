49f73a9333bafd245588435e07ed4b50723c2ddb
David Herman <bitspittle@gmail.com>
2022-02-10 11:34:22 -0800

    Show link anytime user hovers over the entire header line
    Before you had to hover over the link itself to see it.

--- src/jsMain/kotlin/dev/bitspittle/site/components/widgets/navigation/HoverLink.kt
+++ src/jsMain/kotlin/dev/bitspittle/site/components/widgets/navigation/HoverLink.kt
@@ -3,28 +3,38 @@ package dev.bitspittle.site.components.widgets.navigation
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.dom.clearFocus
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.asAttributesBuilder
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.InitSilk
+import com.varabyte.kobweb.silk.InitSilkContext
 import com.varabyte.kobweb.silk.components.icons.fa.FaLink
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.style.*
-import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.registerBaseStyle
 import com.varabyte.kobweb.silk.theme.toSilkPalette
 import kotlinx.browser.document
-import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.em
-import org.jetbrains.compose.web.css.percent
-import org.jetbrains.compose.web.css.vw
-import org.jetbrains.compose.web.dom.Code
-import org.jetbrains.compose.web.dom.Pre
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.css.*
 
-val HoverLinkeStyle = ComponentStyle("bs-hover-link") {
+private val SHOWN_LINK_OPACITY = 80.percent
+
+@InitSilk
+fun initHeaderLinkInteraction(ctx: InitSilkContext) {
+    (2..6).forEach { headingLevel ->
+        // By making the header full width, it means when the user mouses over the entire line they'll see the link
+        ctx.config.registerBaseStyle("h${headingLevel}") {
+            Modifier.fillMaxWidth()
+        }
+        ctx.config.registerBaseStyle("h${headingLevel}:hover > .bs-hover-link") {
+            Modifier.opacity(SHOWN_LINK_OPACITY)
+        }
+    }
+}
+
+
+val HoverLinkStyle = ComponentStyle("bs-hover-link") {
     base { Modifier.opacity(0.percent).transition("opacity .15s").fontSize(0.8.em).margin(left = 0.7.em) }
     link { Modifier.color(colorMode.toSilkPalette().color) }
     visited { Modifier.color(colorMode.toSilkPalette().color) }
-    hover { Modifier.opacity(80.percent) }
-    focus { Modifier.opacity(80.percent) }
+    focus { Modifier.opacity(SHOWN_LINK_OPACITY) }
 }
 
 /**
@@ -32,7 +42,7 @@ val HoverLinkeStyle = ComponentStyle("bs-hover-link") {
  */
 @Composable
 fun HoverLink(href: String, modifier: Modifier = Modifier) {
-    Link(href, HoverLinkeStyle.toModifier().onClick {
+    Link(href, HoverLinkStyle.toModifier().onClick {
         document.activeElement?.clearFocus()
     }.then(modifier)) {
         FaLink()
