57d5fff43277c1ecaeb322ac0636068b5b74b270
David Herman <bitspittle@gmail.com>
2023-01-15 09:53:50 -0800

    Update Kobweb to v0.11.8

--- site/src/jsMain/kotlin/dev/bitspittle/site/components/widgets/button/IconButton.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/site/components/widgets/button/IconButton.kt
@@ -1,6 +1,7 @@
 package dev.bitspittle.site.components.widgets.button
 
 import androidx.compose.runtime.*
+import androidx.compose.web.events.SyntheticMouseEvent
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.BoxScope
 import com.varabyte.kobweb.compose.ui.Modifier
@@ -11,7 +12,7 @@ import com.varabyte.kobweb.silk.theme.shapes.clip
 import org.jetbrains.compose.web.css.*
 
 @Composable
-fun IconButton(onClick: () -> Unit, modifier: Modifier = Modifier, icon: @Composable BoxScope.() -> Unit) {
+fun IconButton(onClick: (SyntheticMouseEvent) -> Unit, modifier: Modifier = Modifier, icon: @Composable BoxScope.() -> Unit) {
     Button(onClick, modifier.padding(0.px).clip(Circle())) {
         Box(Modifier.padding(8.px), content = icon)
     }
