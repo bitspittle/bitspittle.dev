9db8d4d30695f7e34858c747e5333d946c467424
David Herman <bitspittle@gmail.com>
2024-05-14 09:41:33 -0700

    Update Kobweb to v0.18.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 subprojects {
     repositories {
+        mavenLocal()
         mavenCentral()
         google()
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
