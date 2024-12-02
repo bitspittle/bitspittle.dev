78b1cbafbdd469ff7ecaee5d18b9f5488d56b502
David Herman <bitspittle@gmail.com>
2024-12-02 14:02:00 -0800

    Convert "styleModifier" for linear gradient to typed API

--- site/src/jsMain/kotlin/dev/bitspittle/site/components/widgets/blog/koverbadge/GradientBar.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/site/components/widgets/blog/koverbadge/GradientBar.kt
@@ -1,10 +1,12 @@
 package dev.bitspittle.site.components.widgets.blog.koverbadge
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.functions.LinearGradient
+import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.styleModifier
 import org.jetbrains.compose.web.css.*
 
 @Composable
@@ -13,8 +15,6 @@ fun GradientBar() {
         .fillMaxWidth()
         .height(20.px)
         .margin(topBottom = 1.cssRem, leftRight = 0.px)
-        .styleModifier {
-            backgroundImage("linear-gradient(to right, red, green)")
-        }
+        .backgroundImage(linearGradient(LinearGradient.Direction.ToRight, Colors.Red, Colors.Green))
     )
 }
